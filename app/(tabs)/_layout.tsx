5b0c23f58f1659298d6712062b141ef447e7f5ad
Romera02rrs <romera02rrs@gmail.com>
2025-03-28 21:03:39 +0100

    Fixing eas update tabbar issues

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,39 +30,39 @@ export default function TabLayout() {
     >
       <Tabs.Screen
         name="index"
-        options={() => ({
-          title: t("tabs.home"),
+        options={{
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
             <IconSymbol size={29} name="house" color={color} />
           ),
-        })}
+        }}
       />
       <Tabs.Screen
         name="journal-list"
-        options={() => ({
-          title: t("tabs.journal"),
+        options={{
+          title: "Diary",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={29} name="book" color={color} />
           ),
-        })}
+        }}
       />
       <Tabs.Screen
         name="settings"
-        options={() => ({
-          title: t("tabs.settings"),
+        options={{
+          title: "Settings",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="gearshape" color={color} />
           ),
-        })}
+        }}
       />
       <Tabs.Screen
         name="test"
-        options={() => ({
-          title: t("tabs.test"),
+        options={{
+          title: "Test",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="testtube.2" color={color} />
           ),
-        })}
+        }}
       />
     </Tabs>
   );
